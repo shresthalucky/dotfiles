044d7f3de1179f0eaa322469ac6b3649fcdada11
Lucky Shrestha <luckshrestha@gmail.com>
2021-07-02 21:32:31 +0545

    Add dunst

--- .config/fish/functions/sv.fish
+++ .config/fish/functions/sv.fish
@@ -2,6 +2,6 @@
 function sv
   set -g VIRTUALFISH_VERSION 2.5.0
   set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
-  source /home/lucky/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
+  source /home/lucky/.local/lib/python3.9/site-packages/virtualfish/virtual.fish
   emit virtualfish_did_setup_plugins
 end
